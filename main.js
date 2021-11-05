a15b2118625d686050d5e579306d25e307cbe852
Gencehan <gencehangurcam@gmail.com>
2021-11-05 14:19:21 +0100

    project init

---
+++ main.js
@@ -0,0 +1,7 @@
+let redPurple = document.getElementsByClassName("redPurple")
+console.log(redPurple);
+let redPurple2 = document.querySelectorAll(".redPurple")
+console.log(redPurple2);
+
+//3
+let recupH1 = document.querySelectorAll("h1.redPurple")
